5aa0934c95b69bda1fbef86a3a82c4e78c506f77
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-05-24 00:58:36 +0900

    Java언어 BAEKJOON 1001번 문제 풀기!(1)

---
+++ Java_1001_1.java
@@ -0,0 +1,23 @@
+/*
+두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+첫째 줄에 A-B를 출력한다.
+*/
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int A, B;
+        
+        A=scanner.nextInt();
+        B=scanner.nextInt();
+        
+        System.out.println(A-B);
+        
+        scanner.close();
+    }
+}
